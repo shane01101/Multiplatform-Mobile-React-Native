637c59f37bd690ceb4e5c7cd182c9c899340841c
Shane Honanie <shaneh425@gmail.com>
2018-10-27 16:06:32 -0700

    Assignment 01

---
+++ components/ContactComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Contact extends Component {
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    };
+
+    render() {
+        return(
+            <Card title="Contact Information" >
+                <Text style={{margin: 10}}>
+                    121, Clear Water Bay Road{"\n\n"}
+                    Clear Water Bay, Kowloon{"\n\n"}
+                    HONG KONG{"\n\n"}
+                    Tel: +852 1234 5678{"\n\n"}
+                    Fax: +852 8765 4321{"\n\n"}
+                    Email:confusion@food.net{"\n\n"}
+                </Text>
+            </Card>
+        );
+    }
+}
+
+export default Contact;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
@@ -35,21 +37,63 @@ const HomeNavigator = createStackNavigator({
     }
 });
 
+const ContactNavigator = createStackNavigator({
+    Contact: { screen: Contact },
+}, {
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+});
+
+const AboutNavigator = createStackNavigator({
+    About: { screen: About },
+}, {
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+});
+
 const MainNavigator = createDrawerNavigator({
     Home: 
-      { screen: HomeNavigator,
-        navigationOptions: {
-          title: 'Home',
-          drawerLabel: 'Home'
-        }
-      },
+        { screen: HomeNavigator,
+            navigationOptions: {
+                title: 'Home',
+                drawerLabel: 'Home'
+            }
+        },
+    About: 
+        { screen: AboutNavigator,
+            navigationOptions: {
+                title: 'About Us',
+                drawerLabel: 'About Us'
+            }, 
+        },
     Menu: 
-      { screen: MenuNavigator,
-        navigationOptions: {
-          title: 'Menu',
-          drawerLabel: 'Menu'
+        { screen: MenuNavigator,
+            navigationOptions: {
+                title: 'Menu',
+                drawerLabel: 'Menu'
+            }, 
+        },
+    Contact: 
+        { screen: ContactNavigator,
+            navigationOptions: {
+                title: 'Contact Us',
+                drawerLabel: 'Contact Us'
         }, 
-      }
+    }
 }, {
     drawerBackgroundColor: '#D1C4E9'
 });
