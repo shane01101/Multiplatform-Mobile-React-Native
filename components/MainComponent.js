c55467b2a6ec1a6493f3d8ff3490e71bfb788c1b
Shane Honanie <shaneh425@gmail.com>
2018-10-31 16:47:11 -0700

    Connect Redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,7 +6,26 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
+import { connect } from 'react-redux';
+import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionsCreators';
 import { Icon } from 'react-native-elements';
+
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }
+}
+  
+const mapDispatchToProps = dispatch => ({
+    fetchDishes: () => dispatch(fetchDishes()),
+    fetchComments: () => dispatch(fetchComments()),
+    fetchPromos: () => dispatch(fetchPromos()),
+    fetchLeaders: () => dispatch(fetchLeaders()),
+})
+
 
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu,
@@ -168,6 +187,12 @@ const MainNavigator = createDrawerNavigator({
 });
 
 class Main extends Component {
+    componentDidMount() {
+        this.props.fetchDishes();
+        this.props.fetchComments();
+        this.props.fetchPromos();
+        this.props.fetchLeaders();
+    }
 
     render() {
         return (
@@ -202,4 +227,4 @@ const styles = StyleSheet.create({
     }
   });
   
-export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
