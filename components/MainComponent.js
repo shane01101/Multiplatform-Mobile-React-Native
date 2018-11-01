b939f8747794f618334ede9232db5d54ea9fad6a
Shane Honanie <shaneh425@gmail.com>
2018-11-01 13:41:41 -0700

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, Image, StyleSheet, ScrollView, Text } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { connect } from 'react-redux';
@@ -103,6 +104,24 @@ const AboutNavigator = createStackNavigator({
     })
 });
 
+const ReservationNavigator = createStackNavigator({
+    Reservation: { screen: Reservation },
+}, {
+    navigationOptions: ({ navigation }) => ({
+        headerStyle: {
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        },
+        headerLeft: <Icon name='menu' size={24}
+            color='white' 
+            onPress={() => navigation.toggleDrawer()}
+        />
+    })
+});
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
         <SafeAreaView style={styles.container} 
@@ -176,6 +195,21 @@ const MainNavigator = createDrawerNavigator({
                         name='address-card'
                         type='font-awesome'
                         size={22}
+                        color={tintColor}
+                    />
+                )
+        }, 
+    },
+    Reservation: 
+        { screen: ReservationNavigator,
+            navigationOptions: {
+                title: 'Reserve Table',
+                drawerLabel: 'Reserve Table',
+                drawerIcon: ({ tintColor}) => (
+                    <Icon 
+                        name='cutlery'
+                        type='font-awesome'
+                        size={24}
                         color={tintColor}
                     />
                 )
